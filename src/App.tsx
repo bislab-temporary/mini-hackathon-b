0695a957910bc4ead8f48b90d158fdf6c426df6c
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-07 01:37:30 +0000

    chore: debug 用のデフォルトタスクを追加
    動作を確認するため

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ import InputBar from './components/InputBar';
 import TodoLists from './components/TodoLists';
 
 const App = (): JSX.Element => {
-  const [todoItems, setTodoItems] = useState<string[]>([]);
-  const [todoStatus, setTodoStatus] = useState<boolean[]>([]);
-  const [doingItems, setDoingItems] = useState<string[]>([]);
-  const [doingStatus, setDoingStatus] = useState<boolean[]>([]);
-  const [doneItems, setDoneItems] = useState<string[]>([]);
-  const [doneStatus, setDoneStatus] = useState<boolean[]>([]);
+  const [todoItems, setTodoItems] = useState<string[]>(['Todo1', 'Todo2', 'Todo3']);
+  const [todoStatus, setTodoStatus] = useState<boolean[]>([false, false, false]);
+  const [doingItems, setDoingItems] = useState<string[]>(['Doing1', 'Doing2', 'Doing3']);
+  const [doingStatus, setDoingStatus] = useState<boolean[]>([false, false, false]);
+  const [doneItems, setDoneItems] = useState<string[]>(['Done1', 'Done2', 'Done3']);
+  const [doneStatus, setDoneStatus] = useState<boolean[]>([false, false, false]);
 
   return (
     <>
