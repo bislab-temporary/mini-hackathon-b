35aae330acada2a52e3b80062bace59c5d557d2b
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-07 07:10:36 +0000

    feat: Todo 移動時の処理を作成

--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -1,61 +1,91 @@
 import { HStack } from '@chakra-ui/layout';
 import { Dispatch, SetStateAction } from 'react';
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, DraggableLocation, DropResult } from 'react-beautiful-dnd';
 
 import TodoList from './TodoList';
 
+import { TodoType } from '@/types/TodoType';
+
 type Props = {
-  todoItems: string[];
-  todoStatus: boolean[];
-  setTodoStatus: Dispatch<SetStateAction<boolean[]>>;
-  doingItems: string[];
-  doingStatus: boolean[];
-  setDoingStatus: Dispatch<SetStateAction<boolean[]>>;
-  doneItems: string[];
-  doneStatus: boolean[];
-  setDoneStatus: Dispatch<SetStateAction<boolean[]>>;
+  todoList: TodoType[];
+  setTodoList: Dispatch<SetStateAction<TodoType[]>>;
+  doingList: TodoType[];
+  setDoingList: Dispatch<SetStateAction<TodoType[]>>;
+  doneList: TodoType[];
+  setDoneList: Dispatch<SetStateAction<TodoType[]>>;
 };
 
 const TodoLists = ({
-  todoItems,
-  todoStatus,
-  setTodoStatus,
-  doingItems,
-  doingStatus,
-  setDoingStatus,
-  doneItems,
-  doneStatus,
-  setDoneStatus,
+  todoList,
+  setTodoList,
+  doingList,
+  setDoingList,
+  doneList,
+  setDoneList,
 }: Props) => {
+  const getList = (droppableId: string): [TodoType[], Dispatch<SetStateAction<TodoType[]>>] => {
+    if (droppableId === 'Todo') {
+      return [todoList, setTodoList];
+    } else if (droppableId === 'Doing') {
+      return [doingList, setDoingList];
+    } else {
+      return [doneList, setDoneList];
+    }
+  };
+
+  const reorder = (list: TodoType[], startIndex: number, endIndex: number): TodoType[] => {
+    const result = [...list];
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const move = (
+    source: TodoType[],
+    destination: TodoType[],
+    droppableSource: DraggableLocation,
+    droppableDestination: DraggableLocation
+  ): [TodoType[], TodoType[]] => {
+    const sourceClone = [...source];
+    const destClone = [...destination];
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+    destClone.splice(droppableDestination.index, 0, removed);
+    return [sourceClone, destClone];
+  };
+
   const onDragEnd = (result: DropResult) => {
-    if (!result.destination) {
+    const { source, destination } = result;
+
+    if (!destination) {
       // ドロップ対象領域外にドロップした場合
       return;
     }
-    // TODO: タスクの移動処理を記述
+    if (source.droppableId === destination.droppableId) {
+      // 同じリスト内で移動した場合
+      const [list, setList] = getList(source.droppableId);
+      const newList = reorder(list, source.index, destination.index);
+      setList(newList);
+    } else {
+      // 異なるリスト間で移動した場合
+      const [sourceList, setSourcelist] = getList(source.droppableId);
+      const [destinationList, setDestinationList] = getList(destination.droppableId);
+      const [newSourceList, newDestinationList]: [TodoType[], TodoType[]] = move(
+        sourceList,
+        destinationList,
+        source,
+        destination
+      );
+      setSourcelist(newSourceList);
+      setDestinationList(newDestinationList);
+    }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <HStack spacing="24px" overflowX="scroll" align="start">
-        <TodoList
-          statusName="Todo"
-          todoItems={todoItems}
-          todoStatus={todoStatus}
-          setTodoStatus={setTodoStatus}
-        />
-        <TodoList
-          statusName="Doing"
-          todoItems={doingItems}
-          todoStatus={doingStatus}
-          setTodoStatus={setDoingStatus}
-        />
-        <TodoList
-          statusName="Done"
-          todoItems={doneItems}
-          todoStatus={doneStatus}
-          setTodoStatus={setDoneStatus}
-        />
+        <TodoList statusName="Todo" todoList={todoList} setTodoList={setTodoList} />
+        <TodoList statusName="Doing" doingList={doingList} setDoingList={setDoingList} />
+        <TodoList statusName="Done" doneList={doneList} setDoneList={setDoingList} />
       </HStack>
     </DragDropContext>
   );
