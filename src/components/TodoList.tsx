184ec2a8ec237ec000b68290246623dce5bd06da
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-07 08:55:07 +0000

    feat: Drag & Drop 機能の追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -13,6 +13,7 @@ import {
   useEditableControls,
 } from '@chakra-ui/react';
 import React, { Dispatch, SetStateAction } from 'react';
+import { Draggable, DraggableProvided, Droppable, DroppableProvided } from 'react-beautiful-dnd';
 
 import { TodoType } from '@/types/TodoType';
 
@@ -22,7 +23,6 @@ type Props = {
   setTodoList: Dispatch<SetStateAction<TodoType[]>>;
 };
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const TodoList = ({ listName, todoList, setTodoList }: Props) => {
   const handleTodoStatus = (no: number) => {
     const newTodoList: TodoType[] = [];
@@ -62,28 +62,50 @@ const TodoList = ({ listName, todoList, setTodoList }: Props) => {
   };
 
   return (
-    <VStack>
-      {todoList.map((todoItem: TodoType, index: number) => (
-        <Box key={todoItem.no} w="500px" h="60px" p={5} shadow="md" borderWidth="1px">
-          <HStack w="100%" h="100%" justifyContent="space-between">
-            <Checkbox value={index} onChange={() => handleTodoStatus(todoItem.no)} />
-            <Editable defaultValue={todoItem.item} isPreviewFocusable={false}>
-              {todoItem.status === false ? <EditablePreview /> : <EditablePreview as="del" />}
-              <EditableInput />
-              <EditableControls />
-            </Editable>
-            <IconButton
-              colorScheme="red"
-              size="xs"
-              aria-label="Delete"
-              icon={<CloseIcon />}
-              value={index}
-              onClick={() => handleDeleteTodoItem(todoItem.no)}
-            />
-          </HStack>
-        </Box>
-      ))}
-    </VStack>
+    <Droppable droppableId={listName}>
+      {(provided: DroppableProvided) => (
+        <VStack {...provided.droppableProps} ref={provided.innerRef}>
+          {todoList.map((todoItem: TodoType, index: number) => (
+            <Draggable key={todoItem.no} draggableId={String(todoItem.no)} index={index}>
+              {(provided: DraggableProvided) => (
+                <Box
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+                  key={todoItem.no}
+                  w="500px"
+                  h="60px"
+                  p={5}
+                  shadow="md"
+                  borderWidth="1px"
+                >
+                  <HStack w="100%" h="100%" justifyContent="space-between">
+                    <Checkbox value={index} onChange={() => handleTodoStatus(todoItem.no)} />
+                    <Editable defaultValue={todoItem.item} isPreviewFocusable={false}>
+                      {todoItem.status === false ? (
+                        <EditablePreview />
+                      ) : (
+                        <EditablePreview as="del" />
+                      )}
+                      <EditableInput />
+                      <EditableControls />
+                    </Editable>
+                    <IconButton
+                      colorScheme="red"
+                      size="xs"
+                      aria-label="Delete"
+                      icon={<CloseIcon />}
+                      value={index}
+                      onClick={() => handleDeleteTodoItem(todoItem.no)}
+                    />
+                  </HStack>
+                </Box>
+              )}
+            </Draggable>
+          ))}
+        </VStack>
+      )}
+    </Droppable>
   );
 };
 
