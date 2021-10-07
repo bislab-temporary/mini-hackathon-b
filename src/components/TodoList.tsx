8539f1b06a3bcba97b9dfc24ca688835ab2566de
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-07 03:53:23 +0000

    feat: タスクのドラッグを実装 (ただしどこにドロップしても元の位置に戻る)

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,11 @@
 import { VStack, HStack, Checkbox, Box } from '@chakra-ui/react';
 import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
+import { Draggable, DraggableProvided, Droppable, DroppableProvided } from 'react-beautiful-dnd';
 
 import CustomButton from './CustomButton';
 
 type Props = {
+  statusName: string;
   todoItems: string[];
   todoStatus: boolean[];
   setTodoStatus: Dispatch<SetStateAction<boolean[]>>;
@@ -18,16 +20,35 @@ const TodoList = (props: Props) => {
   };
 
   return (
-    <VStack>
-      {props.todoItems.map((todoItem, index) => (
-        <Box key={todoItem} w="500px" h="50px" p={5} shadow="md" borderWidth="1px">
-          <HStack>
-            <Checkbox value={index} onChange={handleTodoStatus} />
-            <CustomButton isDone={props.todoStatus[index]}>{todoItem}</CustomButton>
-          </HStack>
-        </Box>
-      ))}
-    </VStack>
+    <Droppable droppableId={props.statusName}>
+      {(provided: DroppableProvided) => (
+        <VStack {...provided.droppableProps} ref={provided.innerRef}>
+          {props.todoItems.map((todoItem, index) => (
+            <Draggable key={index} draggableId={props.statusName + String(index)} index={index}>
+              {(provided: DraggableProvided) => (
+                <Box
+                  key={todoItem}
+                  w="500px"
+                  h="50px"
+                  p={5}
+                  shadow="md"
+                  borderWidth="1px"
+                  {...provided.draggableProps}
+                  {...provided.dragHandleProps}
+                  ref={provided.innerRef}
+                >
+                  <HStack>
+                    <Checkbox value={index} onChange={handleTodoStatus} />
+                    <CustomButton isDone={props.todoStatus[index]}>{todoItem}</CustomButton>
+                  </HStack>
+                </Box>
+              )}
+            </Draggable>
+          ))}
+          {provided.placeholder}
+        </VStack>
+      )}
+    </Droppable>
   );
 };
 
--- src/components/TodoLists.tsx
+++ src/components/TodoLists.tsx
@@ -1,5 +1,6 @@
 import { HStack } from '@chakra-ui/layout';
 import { Dispatch, SetStateAction } from 'react';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 
 import TodoList from './TodoList';
 
@@ -26,12 +27,37 @@ const TodoLists = ({
   doneStatus,
   setDoneStatus,
 }: Props) => {
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      // ドロップ対象領域外にドロップした場合
+      return;
+    }
+    // TODO: タスクの移動処理を記述
+  };
+
   return (
-    <HStack spacing="24px" overflowX="scroll" align="start">
-      <TodoList todoItems={todoItems} todoStatus={todoStatus} setTodoStatus={setTodoStatus} />
-      <TodoList todoItems={doingItems} todoStatus={doingStatus} setTodoStatus={setDoingStatus} />
-      <TodoList todoItems={doneItems} todoStatus={doneStatus} setTodoStatus={setDoneStatus} />
-    </HStack>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <HStack spacing="24px" overflowX="scroll" align="start">
+        <TodoList
+          statusName="Todo"
+          todoItems={todoItems}
+          todoStatus={todoStatus}
+          setTodoStatus={setTodoStatus}
+        />
+        <TodoList
+          statusName="Doing"
+          todoItems={doingItems}
+          todoStatus={doingStatus}
+          setTodoStatus={setDoingStatus}
+        />
+        <TodoList
+          statusName="Done"
+          todoItems={doneItems}
+          todoStatus={doneStatus}
+          setTodoStatus={setDoneStatus}
+        />
+      </HStack>
+    </DragDropContext>
   );
 };
 
