6a216b7a0671d7b349c45a38107493f8ed78ded2
Morisaki Mikiya <git@morisakimikiya.com>
2021-10-07 01:23:03 +0000

    fix: 新規タスクを Todo にのみ追加するように修正
    自動的に Todo に挿入する仕様にするため

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { HStack } from '@chakra-ui/layout';
 import React, { useState } from 'react';
 
@@ -7,6 +8,10 @@ import TodoItems from './components/TodoItems';
 const App = (): JSX.Element => {
   const [todoItems, setTodoItems] = useState<string[]>([]);
   const [todoStatus, setTodoStatus] = useState<boolean[]>([]);
+  const [doingItems, setDoingItems] = useState<string[]>([]);
+  const [doingStatus, setDoingStatus] = useState<boolean[]>([]);
+  const [doneItems, setDoneItems] = useState<string[]>([]);
+  const [doneStatus, setDoneStatus] = useState<boolean[]>([]);
 
   return (
     <>
@@ -18,8 +23,8 @@ const App = (): JSX.Element => {
       />
       <HStack spacing="24px" overflowX="scroll">
         <TodoItems todoItems={todoItems} todoStatus={todoStatus} setTodoStatus={setTodoStatus} />
-        <TodoItems todoItems={todoItems} todoStatus={todoStatus} setTodoStatus={setTodoStatus} />
-        <TodoItems todoItems={todoItems} todoStatus={todoStatus} setTodoStatus={setTodoStatus} />
+        <TodoItems todoItems={doingItems} todoStatus={doingStatus} setTodoStatus={setDoingStatus} />
+        <TodoItems todoItems={doneItems} todoStatus={doneStatus} setTodoStatus={setDoneStatus} />
       </HStack>
     </>
   );
